873409db9bd29dfb4ff0ec72211d576b45953f52
HopeFrost <hopefrost14@gmail.com>
2019-10-28 18:13:35 -0400

    initial commit

--- ruby_challenges/class_play.rb
+++ ruby_challenges/class_play.rb
@@ -1,88 +1,67 @@
 class Family
-  def set_name= (name)
-    @name = name
-  end
-  def get_name
-    return @name
-  end
-  def set_fav_activity= (fav_activity)
-    @activity = fav_activity
-  end
-  def get_fav_activity
-    return @activity
-  end
+  attr_accessor :name, :fav_activity
+
 end
 
 
 class Pet <Family
-  def set_type= (creature_type)
-    @creature_type = creature_type
-  end
-  def get_type
-    return @creature_type
-  end
-  def set_fav_toy= (fav_toy)
-    @fav_toy = fav_toy
-  end
-  def get_toy
-    return @fav_toy
-  end
+  attr_accessor :type, :fav_toy
 end
   pet1 = Pet.new
-  pet1.set_name= "Kenzy"
-  pet1_name = pet1.get_name
-  pet1.set_type= "dog"
-  pet1_type = pet1.get_type
-  pet1.set_fav_toy= "Lambchop"
-  pet1_toy = pet1.get_toy
+  pet1.name= "Kenzy"
+  pet1_name = pet1.name
+  pet1.type= "dog"
+  pet1_type = pet1.type
+  pet1.fav_toy= "Lambchop"
+  pet1_toy = pet1.fav_toy
 
   pet2 = Pet.new
-  pet2.set_name= "Bear"
-  pet2_name = pet2.get_name
-  pet2.set_type= "cat"
-  pet2_type = pet2.get_type
-  pet2.set_fav_toy= "spinny ball"
-  pet2_toy = pet2.get_toy
+  pet2.name= "Bear"
+  pet2_name = pet2.name
+  pet2.type= "cat"
+  pet2_type = pet2.type
+  pet2.fav_toy= "spinny ball"
+  pet2_toy = pet2.fav_toy
 
   brother1 = Family.new
-  brother1.set_name= "Gordon"
-  brother1_name = brother1.get_name
-  brother1.set_fav_activity= "imersonate Elvis"
-  brother1_activity = brother1.get_fav_activity
+  brother1.name= "Gordon"
+  brother1_name = brother1.name
+  brother1.fav_activity= "imersonate Elvis"
+  brother1_activity = brother1.fav_activity
 
   brother2 = Family.new
-  brother2.set_name= "Daniel"
-  brother2_name = brother2.get_name
-  brother2.set_fav_activity= "hunt"
-  brother2_activity = brother2.get_fav_activity
+  brother2.name= "Daniel"
+  brother2_name = brother2.name
+  brother2.fav_activity= "hunt"
+  brother2_activity = brother2.fav_activity
 
   brother3 = Family.new
-  brother3.set_name= "Peter"
-  brother3_name = brother3.get_name
-  brother3.set_fav_activity= "drink maté"
-  brother3_activity = brother3.get_fav_activity
+  brother3.name= "Peter"
+  brother3_name = brother3.name
+  brother3.fav_activity= "drink maté"
+  brother3_activity = brother3.fav_activity
 
   brother4 = Family.new
-  brother4.set_name= "Anthony"
-  brother4_name = brother4.get_name
-  brother4.set_fav_activity= "pace while he reads"
-  brother4_activity = brother4.get_fav_activity
+  brother4.name= "Anthony"
+  brother4_name = brother4.name
+  brother4.fav_activity= "pace while he reads"
+  brother4_activity = brother4.fav_activity
 
   dad = Family.new
-  dad.set_name= "Jeff"
-  dad_name = dad.get_name
+  dad.name= "Jeff"
+  dad_name = dad.name
 
   nana = Family.new
-  nana.set_name= "Rosemarrie"
-  nana_name = nana.get_name
-  nana.set_fav_activity= "shopping"
-  nana_activity = nana.get_fav_activity
+  nana.name= "Rosemarrie"
+  nana_name = nana.name
+  nana.fav_activity= "shopping"
+  nana_activity = nana.fav_activity
 
   mom = Family.new
-  mom.set_name= "Marsha"
-  mom_name = mom.get_name
-  mom.set_fav_activity= "garden"
-  mom_activity = mom.get_fav_activity
+  mom.name= "Marsha"
+  mom_name = mom.name
+  mom.fav_activity= "garden"
+  mom_activity = mom.fav_activity
 
 puts "#{pet1_name} is a #{pet1_type}. #{pet1_name} favoit toy is #{pet1_toy}."
 puts pet1.inspect
--- ruby_challenges/numerology.rb
+++ ruby_challenges/numerology.rb
@@ -17,30 +17,30 @@ end
 def statment(number)
   case number
   when 1
-    path = puts "One is the leader. The number one indicates the ability to stand alone and is a strong vibration. Ruled by the Sun."
+    path = "One is the leader. The number one indicates the ability to stand alone and is a strong vibration. Ruled by the Sun."
   when 2
-    path = puts "This is the mediator and peace-lover. The number two indicates the desire for harmony. It is a gentle, considerate, and sensitive vibration. Ruled by the Moon."
+    path = "This is the mediator and peace-lover. The number two indicates the desire for harmony. It is a gentle, considerate, and sensitive vibration. Ruled by the Moon."
   when 3
-    path = puts "Number Three is a sociable, friendly, and outgoing vibration. Kind, positive, and optimistic, Three's enjoy life and have a good sense of humor. Ruled by Jupiter."
+    path = "Number Three is a sociable, friendly, and outgoing vibration. Kind, positive, and optimistic, Three's enjoy life and have a good sense of humor. Ruled by Jupiter."
   when 4
-    path = puts "This is the worker. Practical, with a love of detail, Fours are trustworthy, hard-working, and helpful. Ruled by Uranus."
+    path = "This is the worker. Practical, with a love of detail, Fours are trustworthy, hard-working, and helpful. Ruled by Uranus."
   when 5
-    path = puts "This is the freedom lover. The number five is an intellectual vibration. These are 'idea' people with a love of variety and the ability to adapt to most situations. Ruled by Mercury."
+    path =  "This is the freedom lover. The number five is an intellectual vibration. These are 'idea' people with a love of variety and the ability to adapt to most situations. Ruled by Mercury."
   when 6
-    path = puts "This is the peace lover. The number six is a loving, stable, and harmonious vibration. Ruled by Venus."
+    path = "This is the peace lover. The number six is a loving, stable, and harmonious vibration. Ruled by Venus."
   when 7
-    path = puts "This is the deep thinker. The number seven is a spiritual vibration. These people are not very attached to material things, are introspective, and generally quiet. Ruled by Neptune."
+    path = "This is the deep thinker. The number seven is a spiritual vibration. These people are not very attached to material things, are introspective, and generally quiet. Ruled by Neptune."
   when 8
-    path = puts "This is the manager. Number Eight is a strong, successful, and material vibration. Ruled by Saturn."
+    path = "This is the manager. Number Eight is a strong, successful, and material vibration. Ruled by Saturn."
   when 9
-    path = puts "This is the teacher. Number Nine is a tolerant, somewhat impractical, and sympathetic vibration. Ruled by Mars."
+    path = "This is the teacher. Number Nine is a tolerant, somewhat impractical, and sympathetic vibration. Ruled by Mars."
   else
-    path = puts "Hmm.. somthing went wronge."
+    path = "Hmm.. somthing went wronge."
   end
   return path
 end
 puts "please enter your birthdate in MMDDYYYY format:"
 birthdate = gets
 number = birth_path(birthdate)
-meaning = statment(number)
-puts "Your birth path number is #{number}! #{meaning}"
+path = statment(number)
+puts "Your birth path number is #{number}! #{path}"
